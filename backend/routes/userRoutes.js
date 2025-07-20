2cebc6ff513f9173cfd37305e71213a2502dde20
student izaz <izazahmed109@gmail.com>
2025-07-21 00:51:30 +0530

    connect frontend to backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { getAllUsers, getUserProfile } = require('../controllers/userController');
-const Protect = require("../middleware/authMiddleware");
+// const Protect = require("../middleware/authMiddleware");
 
 router.get('/', getAllUsers);
-router.get('/me', Protect, getUserProfile);
+router.get('/me', getUserProfile);
 
 module.exports = router;
